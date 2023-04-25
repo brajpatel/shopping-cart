4b2005f15591b337a4ffa2a4d3dcc9f0efaebe6f
brajpatel <brajeshpatel0002@gmail.com>
2023-04-25 14:43:47 +0100

    Style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer>
-            <h2>Footer</h2>
+            <p>If you liked this site, feel free to check out my github <a href="https://github.com/brajpatel" target="_blank">here<i class="fa-brands fa-github"></i></a></p>
         </footer>
     )
 }
