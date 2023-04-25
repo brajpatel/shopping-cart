796ae1a31d568ac2bdeab846a72949831195cd2f
brajpatel <brajeshpatel0002@gmail.com>
2023-04-25 14:27:15 +0100

    Add home background image and text

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function Home() {
     return (
-        <div>
-            <h2>Home</h2>
+        <div className="home-background">
+            <div className="home-inner">
+                <h2>The Hoodies That Let <span>You</span> Feel Like <span>You</span></h2>
+                <p>I like my money right where I can see it... hanging in my closet</p>
+            </div>
         </div>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 function Nav() {
     return (
         <nav>
-            <h1>Welcome to <span>Shop</span></h1>
+            <h1>Welcome to <span>Hoodies</span></h1>
             <ul>
                 <Link className="page-link" to='/'>Home</Link>
                 <Link className="page-link" to='/shop'>Shop</Link>
-                <Link className="page-link" to='/cart'><i class="fa-solid fa-cart-shopping"></i><span className="cart-size">0</span></Link>
+                <Link className="page-link" to='/cart'><i className="fa-solid fa-cart-shopping"></i><span className="cart-size">0</span></Link>
             </ul>
         </nav>
     )
