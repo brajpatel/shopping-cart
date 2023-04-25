ccb6c04c1ab21df7ca4c327e222e11baa08960e7
brajpatel <brajeshpatel0002@gmail.com>
2023-04-25 14:37:42 +0100

    Position home page text

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
         <div className="home-background">
             <div className="home-inner">
                 <h2>The Hoodies That Let <span>You</span> Feel Like <span>You</span></h2>
-                <p>I like my money right where I can see it... hanging in my closet</p>
+                <p>"I like my money right where I can see it... hanging in my closet"</p>
             </div>
         </div>
     )
