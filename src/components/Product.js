4f520483fa26c702b9b493972b972ce0627d5a6d
brajpatel <brajeshpatel0002@gmail.com>
2023-04-26 14:48:33 +0100

    Add animation delay to product fade in

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 function Product(props) {
-    const { image, name, price, info, addToCart } = props;
+    const { image, name, price, info, addToCart, delay } = props;
 
     return (
-        <div className="product">
+        <div className="product" style={{ animationDelay: delay }}>
             <img src={image} alt="product" />
             <div className="product-info">
                 <p className="product-card-name">{name}</p>
--- src/components/Products.js
+++ src/components/Products.js
@@ -12,37 +12,43 @@ const allProducts = [
         image: blackHoodie,
         name: 'Black Hoodie',
         price: '£28.99',
-        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac turpis egestas sed tempus urna et pharetra pharetra umbasa.'
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac turpis egestas sed tempus urna et pharetra pharetra umbasa.',
+        delay: '0.2s'
     },
     {
         image: greyHoodie,
         name: 'Grey Hoodie',
         price: '£26.99',
-        info: 'Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Iaculis eu non diam phasellus vestibulum.'
+        info: 'Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Iaculis eu non diam phasellus vestibulum.',
+        delay: '0.4s'
     },
     {
         image: redHoodie,
         name: 'Red Hoodie',
         price: '£26.99',
-        info: 'Gravida quis blandit turpis cursus in hac. an sit amet nulla facilisi morbi tempus iaculis urna. Elementum nisi quis.'
+        info: 'Gravida quis blandit turpis cursus in hac. an sit amet nulla facilisi morbi tempus iaculis urna. Elementum nisi quis.',
+        delay: '0.6s'
     },
     {
         image: blueHoodie,
         name: 'Blue Hoodie',
         price: '£24.99',
-        info: 'Sit amet commodo nulla facilisi. Tincidunt sa placerat duis. Ut placerat orci nulla pellentesque. In nisl nisi scelerisque.'
+        info: 'Sit amet commodo nulla facilisi. Tincidunt sa placerat duis. Ut placerat orci nulla pellentesque. In nisl nisi scelerisque.',
+        delay: '0.8s'
     },
     {
         image: yellowHoodie,
         name: 'Yellow Hoodie',
         price: '£22.99',
-        info: 'Et egestas quis ipsum suspendisse ultrices gravida. Ut aliquam purus sit amet. Sed tempus urna et pharetra pharetra.'
+        info: 'Et egestas quis ipsum suspendisse ultrices gravida. Ut aliquam purus sit amet. Sed tempus urna et pharetra pharetra.',
+        delay: '1s'
     },
     {
         image: pinkHoodie,
         name: 'Pink Hoodie',
         price: '£24.99',
-        info: 'Egestas sed tempus urna et pharetra pharetra massa massa ultricies. Leo integer malesuada maecenas accumsan. Commodo umbasa.'
+        info: 'Egestas sed tempus urna et pharetra pharetra massa massa ultricies. Leo integer malesuada maecenas accumsan. Commodo umbasa.',
+        delay: '1.2s'
     }
 ]
 
@@ -58,7 +64,7 @@ function Products() {
             <h2>Shop Our Products</h2>
             {products.map((product) => {
                 return (
-                    <Product image={product.image} name={product.name} price={product.price} info={product.info} addToCart={addToCart}/>
+                    <Product image={product.image} name={product.name} price={product.price} info={product.info} addToCart={addToCart} delay={product.delay}/>
                 )
             })}
         </div>
