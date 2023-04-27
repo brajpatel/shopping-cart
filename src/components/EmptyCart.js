cbad50e363f19c1f871ded640db82ff5b6e942bc
brajpatel <brajeshpatel0002@gmail.com>
2023-04-27 11:43:54 +0100

    Add content to Empty Cart component

--- src/components/EmptyCart.js
+++ src/components/EmptyCart.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function EmptyCart() {
     return (
-        <div>
-            <h2>Your cart is empty!</h2>
-            
+        <div className="empty-cart">
+            <h2>Your cart is empty!<i class="fa-solid fa-face-surprise"></i></h2>
+            <i className="fa-solid fa-cart-shopping"></i>
+            <Link to='/shop'>Browse Products</Link>
         </div>
     )
 }
