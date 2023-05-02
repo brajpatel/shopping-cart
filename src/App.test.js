a9b0297d3d854ea5eab44980e8911368c1f1d3e0
brajpatel <brajeshpatel0002@gmail.com>
2023-05-02 10:32:45 +0100

    Create tests for adding items to an array onclick

--- src/App.test.js
+++ src/App.test.js
@@ -28,4 +28,34 @@ describe('shopping cart', () => {
 
         expect(onClickMock).toBeCalledTimes(3);
     });
+
+    it('adds an item to cart', () => {
+        const addToCart = () => cart.push('item');
+        const cart = [];
+        
+        const onClickMock = jest.fn(addToCart);
+
+        render(<Shop addToCart={onClickMock}/>);
+        const button = screen.getAllByRole("button", { name: "Add to Cart" });
+        
+        userEvent.click(button[0]);
+
+        expect(cart[0]).toMatch(/item/);
+    });
+    
+    it('adds multiple items to cart', () => {
+        const addToCart = () => cart.push('item');
+        const cart = [];
+
+        const onClickMock = jest.fn(addToCart);
+
+        render(<Shop addToCart={onClickMock}/>);
+        const button = screen.getAllByRole("button", { name: "Add to Cart" });
+        
+        userEvent.click(button[0]);
+        userEvent.click(button[2]);
+        userEvent.click(button[5]);
+
+        expect(cart.length).toEqual(3);
+    });
 })
